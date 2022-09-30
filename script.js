66ebdd869e4d6b363625fde988320c76b3f2c3c4
giorgospitias <giorgospitias@gmail.com>
2022-09-30 15:42:58 +0300

    Made timer buttons

--- script.js
+++ script.js
@@ -5,6 +5,14 @@ const list = document.getElementById("list");
 const input = document.getElementById("input");
 const addBtn = document.getElementById("addBtn");
 
+const workBtn = document.getElementById("work");
+const breakBtn = document.getElementById("break");
+const minutes = document.querySelector(".minutes");
+const seconds = document.querySelector(".seconds");
+const playBtn = document.querySelector(".fa-play");
+const refreshTimerBtn = document.querySelector(".fa-sharp");
+const pauseBtn = document.querySelector(".fa-pause");
+
 // Classes names
 const check = "fa-check-circle";
 const uncheck = "fa-circle-thin";
@@ -13,6 +21,8 @@ const lineThrough = "lineThrough";
 
 let LIST = [];
 let id = 0;
+let running = 0;
+let time = 15;
 
 // add to do function
 
@@ -55,9 +65,75 @@ function add(event) {
   }
   input.value = "";
 }
+// Event listeners for enter and click
 addBtn.addEventListener("click", add);
 input.addEventListener("keypress", function (event) {
   if (event.key === "Enter") {
     add(event);
   }
 });
+
+// complete to do
+function completeToDo(element) {
+  element.classList.toggle(check);
+  element.classList.toggle(uncheck);
+  element.parentNode.querySelector(".text").classList.toggle(lineThrough);
+
+  LIST[element.id].done = LIST[element.id].done ? false : true;
+}
+
+// remove to do
+function removeToDo(element) {
+  element.parentNode.parentNode.removeChild(element.parentNode);
+
+  LIST[element.id].trash = true;
+}
+// target the items created dynamically
+
+list.addEventListener("click", function (event) {
+  const element = event.target; // return the clicked element inside list
+  const elementJob = element.attributes.job.value; // complete or delete
+
+  if (elementJob == "complete") {
+    completeToDo(element);
+  } else if (elementJob == "delete") {
+    removeToDo(element);
+  }
+});
+
+//timer part
+function startPause() {
+  playBtn.classList.toggle("hide");
+  pauseBtn.classList.toggle("hide");
+  if (running == 0) {
+    running = setInterval(function () {
+      time--;
+      timerInitialValue();
+
+      // When 0 seconds, stop timer and log out user
+      if (time === 0) {
+        clearInterval(running);
+        time = 15;
+      }
+    }, 1000);
+  } else {
+    clearInterval(running);
+    running = 0;
+  }
+}
+function timerInitialValue() {
+  const min = String(Math.trunc(time / 60)).padStart(2, 0);
+  const sec = String(time % 60).padStart(2, 0);
+
+  // In each call, print the remaining time to UI
+  minutes.textContent = `${min}`;
+  seconds.textContent = `${sec}`;
+}
+function refresh() {
+  time = 15;
+  clearInterval(running);
+  timerInitialValue();
+}
+playBtn.addEventListener("click", startPause);
+pauseBtn.addEventListener("click", startPause);
+refreshTimerBtn.addEventListener("click", refresh);
